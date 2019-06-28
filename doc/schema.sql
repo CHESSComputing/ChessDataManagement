30545baf32c296bfc3149067a05f8430af2c46c5
Valentin Kuznetsov <vkuznet@gmail.com>
2019-06-28 17:32:17 -0400

    Example of FilesDB schema and parameter file

---
+++ doc/schema.sql
@@ -0,0 +1,30 @@
+--https://stackoverflow.com/questions/18387209/sqlite-syntax-for-creating-table-with-foreign-key
+
+CREATE TABLE experiments (
+ experiment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+ name TEXT NOT NULL
+);
+
+CREATE TABLE tiers (
+ tier_id INTEGER PRIMARY KEY AUTOINCREMENT,
+ name TEXT NOT NULL
+);
+
+CREATE TABLE processing (
+ processing_id INTEGER PRIMARY KEY AUTOINCREMENT,
+ name TEXT NOT NULL
+);
+
+CREATE TABLE datasets (
+ dataset_id INTEGER PRIMARY KEY AUTOINCREMENT,
+ experiment_id integer REFERENCES experiments(experiment_id) ON UPDATE CASCADE,
+ processing_id integer REFERENCES processing(processing_id) ON UPDATE CASCADE,
+ tier_id integer REFERENCES tiers(tier_id) ON UPDATE CASCADE,
+ tstamp INTEGER NOT NULL UNIQUE
+);
+
+CREATE TABLE files (
+ file_id INTEGER PRIMARY KEY,
+ dataset_id INTEGER REFERENCES datasets(dataset_id) ON UPDATE CASCADE,
+ name TEXT NOT NULL
+);
