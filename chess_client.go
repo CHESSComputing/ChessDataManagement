a28c3d27b657c9f8db9758bc5efe276c1f45695e
Valentin Kuznetsov <vkuznet@gmail.com>
2019-09-12 11:59:36 -0400

    Add working version of kerberos credentials for the client

--- chess_client.go
+++ chess_client.go
@@ -12,7 +12,13 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"syscall"
 	"time"
+
+	"golang.org/x/crypto/ssh/terminal"
+	"gopkg.in/jcmturner/gokrb5.v7/client"
+	"gopkg.in/jcmturner/gokrb5.v7/config"
+	"gopkg.in/jcmturner/gokrb5.v7/credentials"
 )
 
 // helper function to exit
@@ -21,51 +27,107 @@ func exit(msg string, err error) {
 	os.Exit(1)
 }
 
+// helper function to return user and password
+func userPassword() (string, string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter Username: ")
+	username, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Enter Password: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println()
+	password := string(bytePassword)
+
+	return strings.TrimSpace(username), strings.TrimSpace(password)
+}
+
+// https://github.com/jcmturner/gokrb5/issues/7
+func kuserFromCache(cacheFile string) (*credentials.Credentials, error) {
+	cfg, err := config.Load("/etc/krb5.conf")
+	ccache, err := credentials.LoadCCache(cacheFile)
+	client, err := client.NewClientFromCCache(ccache, cfg)
+	err = client.Login()
+	if err != nil {
+		return nil, err
+	}
+	return client.Credentials, nil
+
+}
+
+func userTicket() []byte {
+	// get user login/password
+	user, password := userPassword()
+	fname := fmt.Sprintf("krb5_%d_%v", os.Getuid(), time.Now().Unix())
+	tmpFile, err := ioutil.TempFile("/tmp", fname)
+	if err != nil {
+		exit("Unabel to get tmp file", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	cmd := exec.Command("kinit", "-c", tmpFile.Name(), user)
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		msg := fmt.Sprintf("Fail to execute '%v'", cmd)
+		exit(msg, err)
+	}
+
+	// start command execution
+	err = cmd.Start()
+	if err != nil {
+		exit("unable to start kinit", err)
+	}
+
+	// write our input to our pipe for command
+	io.WriteString(stdin, password)
+
+	// explicitly close the input informing command that we're done
+	stdin.Close()
+
+	// wait for command to finish its execution
+	cmd.Wait()
+
+	// read tmp file content and return the ticket
+	ticket, err := ioutil.ReadFile(tmpFile.Name())
+	if err != nil || len(ticket) == 0 {
+		exit("unable to read kerberos credentials", err)
+	}
+	return ticket
+}
+
 // helper function to get kerberos ticket
 func getKerberosTicket(krbFile string) []byte {
-	//     fmt.Printf("krbFile: '%s'\n", krbFile)
 	if krbFile != "" {
+		// read krbFile and check user credentials
+		creds, err := kuserFromCache(krbFile)
+		if err != nil {
+			msg := fmt.Sprintf("unable to read %s", krbFile)
+			exit(msg, err)
+		}
+		if creds.Expired() {
+			exit("user credentials are expired, please obtain new/valid krb file", nil)
+		}
 		ticket, err := ioutil.ReadFile(krbFile)
 		if err != nil {
 			exit("unable to read kerberos credentials", err)
 		}
 		return ticket
 	}
-	fname := fmt.Sprintf("krb5_%d_%v", os.Getuid(), time.Now().Unix())
-	tmpFile, err := ioutil.TempFile("/tmp", fname)
-	if err != nil {
-		exit("Unabel to get tmp file", err)
-	}
-	//     defer os.Remove(tmpFile.Name())
-
-	// get user password
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter your kerberos password: ")
-	password, _ := reader.ReadString('\n')
-
-	cname := fmt.Sprintf("KRB5CCNAME=%s", tmpFile.Name())
-	cmd := exec.Command("kinit", "-c", cname)
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		exit("prompt", err)
-	}
-	defer stdin.Close()
-	err = cmd.Start()
-	if err != nil {
-		exit("unable to run kinit", err)
-	}
-	io.WriteString(stdin, password)
-	cmd.Wait()
-	ticket, err := ioutil.ReadFile(tmpFile.Name())
-	if err != nil {
-		exit("unable to read kerberos credentials", err)
-	}
-	return ticket
+	return userTicket()
 }
 
 // helper function to get url.Values form populated with kerberos info
 func getForm(krbFile string) url.Values {
 	ticket := getKerberosTicket(krbFile)
+	if ticket == nil {
+		exit("unable to obtain valid kerberos ticket", nil)
+	}
 	arr := strings.Split(krbFile, "/")
 	fname := arr[len(arr)-1]
 	form := url.Values{}
