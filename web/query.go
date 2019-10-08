d55b7caf856e280651d14016b47c8c649eca637a
Valentin Kuznetsov <vkuznet@gmail.com>
2019-10-08 09:30:31 -0400

    Add convertType

--- web/query.go
+++ web/query.go
@@ -7,10 +7,41 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"gopkg.in/mgo.v2/bson"
 )
+
+func convertType(val interface{}) interface{} {
+	switch v := val.(type) {
+	case []interface{}:
+		return v
+	case string:
+		if IsInt(fmt.Sprintf("%v", v)) {
+			v, e := strconv.Atoi(v)
+			if e == nil {
+				return v
+			}
+		}
+		if IsFloat(fmt.Sprintf("%v", v)) {
+			v, e := strconv.ParseFloat(v, 64)
+			if e == nil {
+				return v
+			}
+		}
+		if strings.Contains(v, ",") {
+			var out []string
+			for _, vvv := range strings.Split(v, ",") {
+				out = append(out, strings.Trim(vvv, " "))
+			}
+			return out
+		}
+		return val
+	default:
+		return val
+	}
+}
 
 // ParseQuery function provides basic parser for user queries and return
 // results in bson dictionary
@@ -23,7 +54,7 @@ func ParseQuery(query string) bson.M {
 	for _, item := range strings.Split(query, " ") {
 		val := strings.Split(strings.TrimSpace(item), ":")
 		if len(val) == 2 {
-			spec[val[0]] = val[1]
+			spec[val[0]] = convertType(val[1])
 		} else {
 			description = fmt.Sprintf("%s %s", description, val)
 		}
