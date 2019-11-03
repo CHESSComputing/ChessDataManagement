03c5f0bfa4ba9befa8769254bb0314f676f7ab81
Valentin Kuznetsov <vkuznet@gmail.com>
2019-11-03 10:07:49 -0500

    re-order

--- doc/mysql_schema.sql
+++ doc/mysql_schema.sql
@@ -1,20 +1,3 @@
-DROP TABLE IF EXISTS `datasets`;
-CREATE TABLE `datasets` (
-  `dataset_id` int(11) NOT NULL AUTO_INCREMENT,
-  `experiment_id` int(11) NOT NULL,
-  `processing_id` int(11) NOT NULL,
-  `tier_id` int(11) NOT NULL,
-  `tstamp` int(11) NOT NULL,
-  PRIMARY KEY (`dataset_id`),
-  UNIQUE KEY `tstamp` (`tstamp`),
-  KEY `fk_eid` (`experiment_id`),
-  KEY `fk_pid` (`processing_id`),
-  KEY `fk_tid` (`tier_id`),
-  CONSTRAINT `fk_eid` FOREIGN KEY (`experiment_id`) REFERENCES `experiments` (`experiment_id`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_pid` FOREIGN KEY (`processing_id`) REFERENCES `processing` (`processing_id`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_tid` FOREIGN KEY (`tier_id`) REFERENCES `tiers` (`tier_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 --
 -- Table structure for table `experiments`
 --
@@ -24,20 +7,6 @@ CREATE TABLE `experiments` (
   `experiment_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`experiment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `files`
---
-
-DROP TABLE IF EXISTS `files`;
-CREATE TABLE `files` (
-  `file_id` int(11) NOT NULL AUTO_INCREMENT,
-  `dataset_id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`file_id`),
-  KEY `fk_did` (`dataset_id`),
-  CONSTRAINT `fk_did` FOREIGN KEY (`dataset_id`) REFERENCES `datasets` (`dataset_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -61,3 +30,39 @@ CREATE TABLE `tiers` (
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`tier_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `datasets`
+--
+
+DROP TABLE IF EXISTS `datasets`;
+CREATE TABLE `datasets` (
+  `dataset_id` int(11) NOT NULL,
+  `experiment_id` int(11) NOT NULL,
+  `processing_id` int(11) NOT NULL,
+  `tier_id` int(11) NOT NULL,
+  `tstamp` int(11) NOT NULL,
+  PRIMARY KEY (`dataset_id`),
+  UNIQUE KEY `tstamp` (`tstamp`),
+  KEY `fk_eid` (`experiment_id`),
+  KEY `fk_pid` (`processing_id`),
+  KEY `fk_tid` (`tier_id`),
+  CONSTRAINT `fk_eid` FOREIGN KEY (`experiment_id`) REFERENCES `experiments` (`experiment_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_pid` FOREIGN KEY (`processing_id`) REFERENCES `processing` (`processing_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_tid` FOREIGN KEY (`tier_id`) REFERENCES `tiers` (`tier_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files` (
+  `file_id` int(11) NOT NULL AUTO_INCREMENT,
+  `dataset_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`file_id`),
+  KEY `fk_did` (`dataset_id`),
+  CONSTRAINT `fk_did` FOREIGN KEY (`dataset_id`) REFERENCES `datasets` (`dataset_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
